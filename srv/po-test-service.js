61374ee4339331cd82b99dad2a551f0d0ccfb904
Chandan Moolya <cdanm9@gmail.com>
2026-01-07 10:58:00 +0000

    Fix : Removal Of External Services for Tomorrow's Upload

--- srv/po-test-service.js
+++ srv/po-test-service.js
@@ -1,5 +1,9 @@
 const cds=require("@sap/cds")
+const {executeHttpRequest}=require("@sap-cloud-sdk/http-client")
 
-module.exports=cds.service.impl(function(srv){
-    const { POHeaders, POItems } = this.entities;       
+module.exports=cds.service.impl(async function(srv){
+    const { POHeaders, POItems} = this.entities;   
+
+    this.on('error', async req => {
+    });
 })
